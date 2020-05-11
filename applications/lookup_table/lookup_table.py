6c2f5981a3593b31c1a54293a2d1e1324ad750fd
Karim B <mattiakarim@live.it>
2020-05-11 13:33:45 +0200

    Complete some of the challenges for day 4

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,12 +1,23 @@
 import math
 import random
 
+# add a cache to speed up performance
+cache = {}
+
 def slowfun(x, y):
     # TODO: Modify to produce the same results, but much faster
+
+    # if the string is present in the cache
+    if f"{x}**{y}" in cache.keys():
+        # return it to avoid complex calculations
+        return cache[f"{x}**{y}"]
+    # else go through the normal process
     v = math.pow(x, y)
     v = math.factorial(v)
     v //= (x + y)
     v %= 982451653
+    # cache result for next time is retrieved
+    cache[f"{x}**{y}"] = v
 
     return v
 
--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,5 +1,31 @@
 def no_dups(s):
     # Implement me.
+    # initialize a dictionary to store cached values
+    cache = {}
+    # pass to a variable an empty string which will be filled with unique words
+    unique_words = ""
+    # split words in a list
+    list_words = s.split(" ")
+    # for each word in the list
+    for word in list_words:
+        # check if the word is in the cache
+        if word in cache:
+            # if so do nothing and go to next iteration
+            continue
+        else:
+            # else the unique word is not empty
+            if unique_words:
+                # cache the word and save a count as value
+                cache[word] = 1
+                # chain new word to the unique words string
+                unique_words = f"{unique_words} {word}" 
+            else:
+                # else cache the new word
+                cache[word] = 1
+                # set the unique string to be the the word
+                unique_words = word
+    # return the unique words string
+    return unique_words
 
 
 if __name__ == "__main__":
